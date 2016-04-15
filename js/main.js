27547ed98188aec12e85f90061e327ce11171b10
Winnie-Li <winnielee841001@gmail.com>
2016-04-15 12:34:50 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f4a75ba9-7721-4363-884d-c3820b0b917c";
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=190796c8-7c56-42e0-8068-39242b8ec927";
 
 function getData(cb) {
   $.ajax({
